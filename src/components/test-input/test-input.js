866c7dc23f7471bbeb35efb08c4777832955b5c8
jeffchew <jeff.chew@ibm.com>
2023-10-30 12:28:27 -0400

    feat(github): initial github templates

--- src/components/test-input/test-input.js
+++ src/components/test-input/test-input.js
@@ -8,14 +8,45 @@
  */
 
 import settings from '../../globals/settings.js';
-import testInput from './src/test-input.js';
-import { testInputTemplate } from './src/test-input.template.js';
+import { LitElement, html } from 'lit';
+import { SearchTypeaheadAPI } from '../../services/SearchTypeahead/index.js';
 
 const { stablePrefix: c4aiPrefix } = settings;
 
-class C4AITestInput extends testInput {
+/**
+ * Input component using search typeahead api
+ */
+export default class C4AITestInput extends LitElement {
+  static properties = {
+    searchResults: { attribute: false },
+  };
+
+  constructor() {
+    super();
+    this.searchResults;
+  }
+
+  async getResults(searchQuery) {
+    const response = await SearchTypeaheadAPI.getResults(searchQuery);
+    return response.map((res) => res[0]);
+  }
+
+  _handleInput(event) {
+    const { value } = event.target;
+    this.getResults(value).then((res) => {
+      this.searchResults = res;
+    });
+  }
+
   render() {
-    return testInputTemplate(this);
+    const { _handleInput: handleInput } = this;
+    return html`<div>
+      <label>Search typeahead</label>
+      <input type="text" @input="${handleInput}" />
+      ${this.searchResults
+        ? this.searchResults.map((result) => html`<p>${result}</p>`)
+        : undefined}
+    </div>`;
   }
 }
 

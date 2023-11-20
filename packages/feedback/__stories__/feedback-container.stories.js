fbd2afc2c14ce0bc4ed401fb154939463d17fe51
Priyanshu Rai <Priyanshu.Rai@ibm.com>
2023-11-20 20:33:13 +0530

    feat(feedback-container): initial setup

---
+++ packages/feedback/__stories__/feedback-container.stories.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import '../components/feedback-container/feedback-container';
+import { html } from 'lit';
+
+/**
+ * More on how to set up stories at: https://storybook.js.org/docs/web-components/writing-stories/introduction
+ */
+export default {
+  title: 'Components/Feedback container/Feedback container',
+  tags: ['autodocs'],
+};
+
+/**
+ * More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
+ *
+ * @type {{args: {label: string}, render: (function(*): TemplateResult<1>)}}
+ */
+export const Default = {
+  args: {
+    label: 'Feedback container',
+  },
+
+  /**
+   * Renders the template for Storybook
+   * @param {object} args Storybook arguments
+   * @returns {TemplateResult<1>}
+   */
+  render: (args) =>
+    html` <c4ai-feedback-container api_key="" user_id="" model_id="">Hello! This text is inside feedback container. This web component is still in development phase.</c4ai-feedback-container>`,
+};

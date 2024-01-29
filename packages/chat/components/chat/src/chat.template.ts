2d5e2b5cd95ecb9431455182a280ded200bb6cd1
owen cornec <o.cornec@owens-air.myfiosgateway.com>
2024-01-29 08:00:04 -0500

    chore(chat): fixed all TS, CI and lint issues

--- packages/chat/components/chat/src/chat.template.ts
+++ packages/chat/components/chat/src/chat.template.ts
@@ -24,8 +24,8 @@ const { stablePrefix: c4aiPrefix } = settings;
  * @returns {TemplateResult<1>} Lit html template
  */
 export function chatTemplate(customElementClass) {
-  const { 
-    _handleInput: handleInput, 
+  const {
+    _handleInput: handleInput,
     _sendInput: sendInput,
     _setMessageText: setMessageText,
     _messages: messages,
@@ -35,67 +35,99 @@ export function chatTemplate(customElementClass) {
     _handleHoverOut: handleHoverOut,
     _handleEdit: handleEdit,
     _handleFeedback: handleFeedback,
-    _handleAPIselection: handleAPIselection 
+    _handleAPIselection: handleAPIselection,
   } = customElementClass;
 
-    return html`<div class="${c4aiPrefix}--chat-container">
-        <div class="chat-messages">
+  return html`<div class="${c4aiPrefix}--chat-container">
+      <div class="chat-messages">
         &nbsp;
-          ${messages.map(
-            (message,index) => html` 
-              <div class="${message.origin}-message subtype-${message.style}" @mouseout="${(e) => handleHoverOut(index,e,message.origin)}">
-                
-                <div class='message-container'  @mouseover="${(e) => handleHoverIn(index,e,message.origin)}" >
-                <div class='message-time'>${message.time}</div>
-                <div class='message-icon'>
-                    <div class='${message.origin}-icon'>
-                      ${message.origin == "user"
-                        ? User24() : "AI"}
-                    </div>
+        ${messages.map(
+          (message, index) => html` <div
+            class="${message.origin}-message subtype-${message.style}"
+            @mouseout="${(e) => handleHoverOut(index, e, message.origin)}">
+            <div
+              class="message-container"
+              @mouseover="${(e) => handleHoverIn(index, e, message.origin)}">
+              <div class="message-time">${message.time}</div>
+              <div class="message-icon">
+                <div class="${message.origin}-icon">
+                  ${message.origin == 'user' ? User24() : 'AI'}
                 </div>
-                <div class='message-text'>
-                ${message.text}
-                </div>
-
-                </div>
-                <div class="${message.origin}-dropdown">
-                  <div class='small-button'  @click="${(e) => {handleEdit(e,index)}}">
-                     ${Edit16()}
-                  </div>
-                  <div class='small-button' @click="${(e) => {handleFeedback(e,index,"like",message.text)}}">
-                    ${ThumbsUp16()}
-                  </div>
-                  <div class='small-button' @click="${(e) => {handleFeedback(e,index,"dislike",message.text)}}">
-                     ${ThumbsDown16()}
-                  </div>
-                </div>
-              </div>`
-          )}
-          <div class="bot-message ${queryInProgress? '':'hidden'}">
-             <div class='message-container'>
-                    <div class="loading-container">
-                      <div class="loading-bar"></div>
-                    </div>
-             </div>
+              </div>
+              <div class="message-text">${message.text}</div>
+            </div>
+            <div class="${message.origin}-dropdown">
+              <div
+                class="small-button"
+                @click="${(e) => {
+                  handleEdit(e, index);
+                }}">
+                ${Edit16()}
+              </div>
+              <div
+                class="small-button"
+                @click="${(e) => {
+                  handleFeedback(e, index, 'like', message.text);
+                }}">
+                ${ThumbsUp16()}
+              </div>
+              <div
+                class="small-button"
+                @click="${(e) => {
+                  handleFeedback(e, index, 'dislike', message.text);
+                }}">
+                ${ThumbsDown16()}
+              </div>
+            </div>
+          </div>`
+        )}
+        <div class="bot-message ${queryInProgress ? '' : 'hidden'}">
+          <div class="message-container">
+            <div class="loading-container">
+              <div class="loading-bar"></div>
+            </div>
           </div>
         </div>
-         <div class="input-container">
-          <div class='search-icon'>${Search24()}</div>
-           <input type="text"
-           .value="${messageText}"
-           @input="${setMessageText}"
-           @keyup="${handleInput}" />
-            <span class="send-button" @click="${() => sendInput(messageText)}"> ${ArrowRight16()} </span>
-         </div>
       </div>
-      <div class="test-section">
-        <div class="test-options-container">
-          <h2>Test target url</h2>
-          <form id="target-urls">
-            <label><input type="radio" name="option" value="local" checked @input="${handleAPIselection}">Local server</label>
-            <label><input type="radio" name="option" value="bam" @input="${handleAPIselection}">BAM Reasearch service</label>
-            <label><input type="radio" name="option" value="watsonx-ai" @input="${handleAPIselection}">Watsonx-ai API</label>
-          </form>
-        </div>
-      </div>`;
+      <div class="input-container">
+        <div class="search-icon">${Search24()}</div>
+        <input
+          type="text"
+          .value="${messageText}"
+          @input="${setMessageText}"
+          @keyup="${handleInput}" />
+        <span class="send-button" @click="${() => sendInput(messageText)}">
+          ${ArrowRight16()}
+        </span>
+      </div>
+    </div>
+    <div class="test-section">
+      <div class="test-options-container">
+        <h2>Test target url</h2>
+        <form id="target-urls">
+          <label
+            ><input
+              type="radio"
+              name="option"
+              value="local"
+              checked
+              @input="${handleAPIselection}" />Local server</label
+          >
+          <label
+            ><input
+              type="radio"
+              name="option"
+              value="bam"
+              @input="${handleAPIselection}" />BAM Reasearch service</label
+          >
+          <label
+            ><input
+              type="radio"
+              name="option"
+              value="watsonx-ai"
+              @input="${handleAPIselection}" />Watsonx-ai API</label
+          >
+        </form>
+      </div>
+    </div>`;
 }

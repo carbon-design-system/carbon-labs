32b08497f28a7d4becd3b1d7e1eb95135f1e6440
Priyanshu Rai <priyanshu.rai891@gmail.com>
2024-03-13 17:40:10 +0530

    feat(feedback): improvements and fixes for feedback container (#86)
    * feat(feedback): improvements and fixes for feedback container
    
    * fix(feedback): removed hardcoded percentage
    
    * chore(feedback): updated storybook
    
    * feat(feedback): add build script to package
    
    * fix(feedback): fixed minor issues
    
    * fix(feedback): fixed pr comments
    
    * fix(feedback): add .js extensions
    
    ---------
    
    Co-authored-by: Priyanshu Rai <Priyanshu.Rai@ibm.com>
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>

--- packages/feedback/index.ts
+++ packages/feedback/index.ts
@@ -7,7 +7,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import './components/chat/chat.js';
-import './components/message/message.js';
-import './components/card/card.js';
-import './components/header/header.js';
+import './components/feedback/feedback.js';

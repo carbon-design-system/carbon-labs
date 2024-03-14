8d309efc5f988b33f4f21725de1f6aa7c5adf626
ariellalgilmore <ariellalgilmore@gmail.com>
2024-03-14 09:41:47 -0700

    feat(storybook): upgrade to 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 /**
  * @license
@@ -18,9 +19,9 @@ const config = {
     '../packages/**/__stories__/*.mdx',
     '../packages/**/__stories__/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials")],
   framework: {
-    name: '@storybook/web-components-vite',
+    name: getAbsolutePath("@storybook/web-components-vite"),
     options: {},
   },
   docs: {
@@ -49,3 +50,7 @@ const config = {
   },
 };
 export default config;
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}

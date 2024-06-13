c44ff7938a1a9db2e0cc1239350cd3b978dafc25
Lily <ms.lilypeng@gmail.com>
2024-06-13 13:24:59 -0400

    feat(tag): New Tag component (#179)
    * [feat] tag with content control & tooltip on hover
    
    * [fix] content added to template
    
    * [feat] added click event
    
    * [feat] border color control
    
    * [feat] tooltip position & text controls
    
    * [fix] separate folder
    
    * [fix] cleanup unneeded
    
    * [feat] expose tag-click event
    
    * Update yarn.lock
    
    * [fix] code review cleanup
    
    * Update yarn.lock
    
    * Update packages/tag/package.json
    
    * [fix] disable duplicate attributes
    
    * [feat] stackblitz example
    
    * Update packages/tag/components/tag/src/tag.template.ts
    
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>
    
    * Update yarn.lock
    
    * fix(tag): adjust syntax
    
    ---------
    
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>

---
+++ packages/tag/components/tag/src/tag.template.ts
@@ -0,0 +1,45 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit';
+import { settings } from '@carbon-labs/utilities/es/settings/index.js';
+const { stablePrefix: clabsPrefix } = settings;
+
+import '@carbon/web-components/es/components/tooltip/index.js';
+
+/**
+ * Lit template for card
+ *
+ * @param {object} customElementClass Class functionality for the custom element
+ * @returns {TemplateResult<1>} Lit html template
+ */
+export function tagTemplate(customElementClass) {
+  const {
+    text: text,
+    color: color,
+    tooltipPosition: tooltipPosition,
+    tooltipText: tooltipText,
+    handleClick: handleClick,
+  } = customElementClass;
+
+  return html` <div class="${clabsPrefix}--tag">
+    <div class="${clabsPrefix}--tag-container">
+      <cds-tooltip align="${tooltipPosition}">
+        <button
+          class="${clabsPrefix}--tag-button ${clabsPrefix}--tag-tooltip-trigger"
+          color="${color}"
+          tooltip-position="${tooltipPosition}"
+          @click="${handleClick}">
+          <slot>${text}</slot>
+        </button>
+        <cds-tooltip-content id="content"> ${tooltipText} </cds-tooltip-content>
+      </cds-tooltip>
+    </div>
+  </div>`;
+}

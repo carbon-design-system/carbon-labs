908ef0ef00c98a45915432282180f8161d5ae7f5
Lily <lily.peng@ibm.com>
2024-06-27 09:32:26 -0400

    feat(prompt-tuning): new Prompt Tuning component (#201)
    * [feat] modal open on button click
    
    * [feat] modal open close working
    
    * [feat] edit modal html/css
    
    * [feat] task_view import for prompt list
    
    * [feat] semantic search view in list
    
    * [feat] view name and dropdown list
    
    * [feat] property name on hover
    
    * [feat] edit modal working except view
    
    * [feat] view dropdown complete
    
    * [feat] tooltips & inline dropdown
    
    * [feat] Rename, add variables & parameters
    
    * [feat] rename event complete
    
    * [feat] event for closing tag
    
    * [feat] add context variable & parameter events
    
    * [feat] save prompt event
    
    * [feat] delete prompt action event
    
    * [feat] select view event
    
    * [feat] Add new prompt done
    
    * [feat] data as json promptSamples
    
    * [feat] storybook args streamlined
    
    * [feat] parameters moved to right
    
    * [feat] function controls documented
    
    * [feat] add new prompt event
    
    * [fix] improve closing tag
    
    * [fix] linting
    
    * [fix] lint extension
    
    * chore(package): remove changelog and edit version
    
    * chore(prompt-tuning): edit boolean attribute
    
    ---------
    
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>

---
+++ packages/prompt-tuning/components/prompt-tuning/prompt-tuning.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { customElement } from 'lit/decorators.js';
+import { settings } from '@carbon-labs/utilities/es/settings/index.js';
+import { PromptTuning } from './src/prompt-tuning.js';
+import { TemplateResult } from 'lit';
+import { promptTuningTemplate } from './src/prompt-tuning.template.js';
+
+const { stablePrefix: clabsPrefix } = settings;
+
+/**
+ * Component extending the PromptTuning component
+ *
+ * @element clabs-prompt-tuning
+ */
+@customElement(`${clabsPrefix}-prompt-tuning`)
+class CLABSPromptTuning extends PromptTuning {
+  /**
+   * Renders the template while passing in class functionality
+   */
+  render(): TemplateResult<1> {
+    return promptTuningTemplate(this);
+  }
+}
+
+export default CLABSPromptTuning;

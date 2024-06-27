a2c6edb40af713d1ec575fda7dc8c51a211e64ec
Lily Peng <lily.peng@ibm.com>
2024-06-27 13:34:28 -0400

    [fix] expose attributes & rename

--- packages/ux-control/__stories__/ux-control.stories.js
+++ packages/ux-control/__stories__/ux-control.stories.js
@@ -6,7 +6,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import '../components/prompt-tuning/prompt-tuning';
+import '../components/ux-control/ux-control';
 import { html } from 'lit';
 import { action } from '@storybook/addon-actions';
 
@@ -14,8 +14,8 @@ import '@carbon/web-components/es/components/button/index.js';
 import semantic_search_view from './semantic_search_view.json';
 
 export default {
-  title: 'Components/Prompt Tuning/Prompt Tuning',
-  component: 'clabs-prompt-tuning',
+  title: 'Components/UX Control/UX Control',
+  component: 'clabs-ux-control',
 };
 
 const defaultArgs = {
@@ -46,13 +46,12 @@ const defaultArgs = {
   onDeletePrompt: action('delete-prompt'),
   onChangeView: action('change-view'),
   onAddPrompt: action('add-prompt'),
-  isListModalOpen: true,
+  open: true,
 };
 
 /* Default controls */
 const defaultControls = {
-  isListModalOpen: {
-    name: 'open',
+  open: {
     control: { type: 'boolean' },
     description: 'Whether the tuning prompts modal is open or not',
     table: {
@@ -182,7 +181,7 @@ export const Default = {
    * @returns {TemplateResult<1>}
    */
   render: ({
-    isListModalOpen,
+    open,
     promptSamples,
     viewList,
     currentView,
@@ -195,8 +194,8 @@ export const Default = {
     onChangeView,
     onAddPrompt,
   }) =>
-    html` <clabs-prompt-tuning
-      ?isListModalOpen="${isListModalOpen}"
+    html` <clabs-ux-control
+      ?open=${open}
       .promptSamples=${promptSamples}
       .currentView=${currentView}
       .viewList=${viewList}
@@ -207,5 +206,5 @@ export const Default = {
       @save-prompt=${onSavePrompt}
       @delete-prompt=${onDeletePrompt}
       @change-view=${onChangeView}
-      @add-prompt=${onAddPrompt}></clabs-prompt-tuning>`,
+      @add-prompt=${onAddPrompt}></clabs-ux-control>`,
 };

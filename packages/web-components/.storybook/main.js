9d1bfa639806c7c4e1987593dc4802ab5eb6b7a6
kennylam <909118+kennylam@users.noreply.github.com>
2024-11-15 10:42:42 -0500

    chore(project): create framework subdirectory for web components (#303)
    * chore(framework): move wc components to wc packages dir
    
    * chore(sb): move wc sb to wc dir
    
    * chore(framework): move stuff to wc package dir
    
    * chore(project): update clean script
    
    * chore(package): update build script paths
    
    * chore(project): update build; move examples
    
    * chore(package): update build paths
    
    * chore(deps): update build dist
    
    * chore(package): update build dist
    
    * chore(wc): update custom elements manifest
    
    * chore(build): update gulp tasks
    
    * chore(build): move build dist to tasks dir
    
    * chore(build): update build files
    
    * chore(build): update build tasks
    
    * chore(lint): fix lint errors
    
    * chore(gh): update workflows
    
    * chore(sb): update storybook paths
    
    * chore(styles): update carbon styles
    
    * chore(sb): upgrade sb to v8
    
    * chore(package): update package scripts
    
    * chore(styles): fix styles
    
    * chore(storybook): fix story doc file extensions
    
    * chore(build): remove unused files
    
    * chore(gh): update workflow commands
    
    * chore(build): retrigger builds
    
    * chore(examples): move to web components subdir
    
    * chore: move globals folder into src
    
    * chore: remove component folder from export path
    
    * chore: correct the pathing to globals folder
    
    * chore: hide stories for certain components
    
    ---------
    
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>

---
+++ packages/web-components/.storybook/main.js
@@ -0,0 +1,54 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { mergeConfig } from 'vite';
+import postcss from 'rollup-plugin-postcss';
+import postcssLit from 'rollup-plugin-postcss-lit';
+import sass from 'sass';
+
+const config = {
+  stories: [
+    '../src/**/__stories__/*.mdx',
+    '../src/**/__stories__/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            implementation: sass,
+          },
+        },
+      },
+      plugins: [
+        postcss(),
+        postcssLit({
+          include: [
+            '../../../node_modules',
+            'src/**/*.scss',
+            'src/**/*.scss?*',
+          ],
+        }),
+      ],
+    });
+  },
+};
+export default config;
---
+++ packages/web-components/tasks/build-dist.js
@@ -0,0 +1,140 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import fs from 'fs';
+import autoprefixer from 'autoprefixer';
+import commonjs from '@rollup/plugin-commonjs';
+import esbuild from 'rollup-plugin-esbuild';
+import cssnano from 'cssnano';
+import path from 'path';
+import postcss from 'postcss';
+import replace from '@rollup/plugin-replace';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import alias from '@rollup/plugin-alias';
+import rollupPluginLitSCSS from '../tools/rollup-plugin-lit-scss.js';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/**
+ * Gets all of the folders and returns out
+ *
+ * @param {string} dir Directory to check
+ * @returns {string[]} List of folders
+ * @private
+ */
+function _getFolders(dir) {
+  return fs
+    .readdirSync(dir)
+    .filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
+}
+
+const folders = _getFolders(`components`);
+
+for (let i = folders.length - 1; i >= 0; i--) {
+  if (!fs.existsSync(`components/${folders[i]}/${folders[i]}.ts`)) {
+    folders.splice(i, 1);
+  }
+}
+
+/**
+ * Grab current package version
+ */
+function _getVersion() {
+  const data = fs.readFileSync('package.json', 'utf8');
+  const version = /"version": "(.*?)"/g.exec(data);
+  return `v${version[1]}`;
+}
+
+/**
+ * Grab name of package
+ */
+function _getName() {
+  const packagePath = process.cwd();
+  const name = /packages\/web-components\/src\/components\/(.*?)(?=$)/g.exec(
+    packagePath
+  );
+  return name[1];
+}
+
+/**
+ * Generates the multi-input for the rollup config
+ *
+ * @param {Array} folders Package names as inputs
+ * @param {boolean} canary set canary in path
+ * @returns {{}} Object with inputs
+ * @private
+ */
+function _generateInputs(folders, canary) {
+  const inputs = {};
+  const version = _getVersion();
+  const name = _getName();
+
+  folders.forEach((folder) => {
+    // get the main file of each component (ie. components/${component-name}/${component-name}.ts)
+    inputs[
+      `${name}/${canary ? 'canary' : version}/${folder}.min`
+    ] = `components/${folder}/${folder}.ts`;
+  });
+
+  /*
+   * add the packages' index.js file that contains all the component imports
+   * (ie. packages/chat/index.js)
+   */
+  inputs[`${name}/${canary ? 'canary' : version}/index.min`] = `index.ts`;
+
+  return inputs;
+}
+
+/**
+ * Rollup configuration to generate the CDN artifacts
+ * @param {Object} commandLineArgs Rollup CI arguments
+ */
+export default (commandLineArgs) => {
+  return {
+    input: _generateInputs(folders, commandLineArgs.configCanary),
+    output: {
+      format: 'es',
+      dir: 'dist',
+    },
+    plugins: [
+      alias({
+        entries: [{ find: /^(.*)\.scss\?inline$/, replacement: '$1.scss' }],
+      }),
+      replace({
+        'process.env.NODE_ENV': 'production',
+        preventAssignment: true,
+      }),
+      nodeResolve({
+        browser: true,
+        preferBuiltins: false,
+        extensions: ['.js', '.ts'],
+      }),
+      commonjs({
+        include: [/node_modules/],
+        sourceMap: true,
+      }),
+      json(),
+      esbuild({ sourceMap: false, tsconfig: '../tsconfig.json' }),
+      rollupPluginLitSCSS({
+        includePaths: [path.resolve(__dirname, '../../../node_modules')],
+        async preprocessor(contents, id) {
+          return (
+            await postcss([autoprefixer(), cssnano()]).process(contents, {
+              from: id,
+            })
+          ).css;
+        },
+      }),
+      terser(),
+    ],
+  };
+};

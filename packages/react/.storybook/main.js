948ef5970d361011864d881e853972d18360d289
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-12-05 14:25:35 -0500

    chore(storybook): add theming and a11y checker

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -9,14 +9,16 @@ import remarkGfm from 'remark-gfm';
 function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-storysource'),
-    getAbsolutePath('@storybook/addon-webpack5-compiler-babel'),
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-storysource',
+    '@storybook/addon-webpack5-compiler-babel',
+    'storybook-addon-accessibility-checker',
     {
       name: '@storybook/addon-docs',
       options: {

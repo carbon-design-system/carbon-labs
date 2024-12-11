50a59e31f86f9b622e04d65df2fa4a9653f076b9
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-12-11 11:35:17 -0500

    chore: add boilerplate web component

---
+++ packages/web-components/src/components/example-button/__tests__/__snapshots__/extended-button.test.snap.js
@@ -0,0 +1,17 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots[
+  'clabs-extended-button should render with cds-button minimum attributes'
+] = `<clabs-extended-button
+  has-main-content=""
+  kind="primary"
+  size="lg"
+  tooltip-alignment=""
+  tooltip-position="top"
+  type="button"
+>
+  button
+</clabs-extended-button>
+`;
+/* end snapshot clabs-extended-button should render with cds-button minimum attributes */
---
+++ packages/web-components/src/components/example-button/components/example-button/example-button.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { customElement } from 'lit/decorators.js';
+import { settings } from '@carbon-labs/utilities/es/settings/index.js';
+import exampleButton from './src/example-button.template.js';
+
+const { stablePrefix: clabsPrefix } = settings;
+
+/**
+ * Component extending the @carbon/web-components' button
+ */
+@customElement(`${clabsPrefix}-example-button`)
+class CLABSExampleButton extends exampleButton {}
+
+export default CLABSExampleButton;

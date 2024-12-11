5da4b22b59c5eca3365f8a581214988fa8d06f9a
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-12-11 09:49:23 -0500

    chore: clean up for ci-check

--- packages/react/src/components/ExampleButton/components/ExampleButton.tsx
+++ packages/react/src/components/ExampleButton/components/ExampleButton.tsx
@@ -1,3 +1,11 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import React from 'react';
 import { Button, ButtonProps } from '@carbon/react';
 import './button.scss';
@@ -7,6 +15,10 @@ import './button.scss';
 interface ExampleButtonProps extends ButtonProps<any> {
   children?: React.ReactNode;
 }
-export const ExampleButton = ({ children = 'Button', ...rest }: ExampleButtonProps ) => {
+
+export const ExampleButton = ({
+  children = 'Button',
+  ...rest
+}: ExampleButtonProps) => {
   return <Button {...rest}>{children}</Button>;
 };

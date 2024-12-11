1c1fa4dab8fc7f165654f802e4f4609d42052c59
Gururaj J <89023023+Gururajj77@users.noreply.github.com>
2024-12-11 20:26:48 +0530

    chore(storybook): react boilerplate (#324)
    * story: react boilerplate
    
    * chore(storybook): update storybook configs
    
    * chore(storybook): add theming and a11y checker
    
    * added jest test suite and ts supp:
    
    * yarn lock commit
    
    * fix storybook
    
    * added snapshot testing
    
    * chore: update package.jsons and storybook theme url
    
    * chore: point to babelrc from jest config
    
    * Update packages/react/.gitignore
    
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>
    
    * resolved comments
    
    * modified mdx
    
    * yarn lock conflict
    
    * yarn lock main
    
    * lock file fix
    
    * chore: clean up for ci-check
    
    ---------
    
    Co-authored-by: Anna Wen <54281166+annawen1@users.noreply.github.com>

---
+++ packages/react/jest.config.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': ['babel-jest', { configFile: './.babelrc' }],
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  // Test file patterns to match
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
---
+++ packages/react/src/components/ExampleButton/__stories__/ExampleButton.stories.js
@@ -0,0 +1,27 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import mdx from './ExampleButton.mdx';
+import { ExampleButton } from '../components/ExampleButton';
+
+export default {
+  title: 'Components/ExampleButton',
+  component: ExampleButton,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+/**
+ *
+ */
+export const Default = () => <ExampleButton />;

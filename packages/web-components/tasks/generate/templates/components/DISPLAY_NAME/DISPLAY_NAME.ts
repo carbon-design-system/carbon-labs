3c24d375f2b8773f7c4dd704197ae6267d919685
Anna Wen <54281166+annawen1@users.noreply.github.com>
2025-01-06 17:37:03 -0500

    feat: script to generate base component in react + web-components (#354)
    * feat: script to generate base component in react
    
    * chore: clean up script
    
    * chore: license ignore generate template
    
    * chore: script for web components
    
    * chore: script for wc

---
+++ packages/web-components/tasks/generate/templates/components/DISPLAY_NAME/DISPLAY_NAME.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. FULL_YEAR
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { customElement } from 'lit/decorators.js';
+import { settings } from '@carbon-labs/utilities/es/settings/index.js';
+import CAMEL_CASE from './src/DISPLAY_NAME.template.js';
+
+const { stablePrefix: clabsPrefix } = settings;
+
+/**
+ * Component extending the @carbon/web-components' button
+ */
+@customElement(`${clabsPrefix}-DISPLAY_NAME`)
+class CLABSPASCAL_CASE extends CAMEL_CASE {}
+
+export default CLABSPASCAL_CASE;

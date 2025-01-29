ae286a8365254c41af99bf19827221ecd4bd6a32
Anna Wen <54281166+annawen1@users.noreply.github.com>
2025-01-29 10:59:41 -0500

    chore: simplify test

--- packages/react/src/components/TextHighlighter/__tests__/TextHighlighter.test.js
+++ packages/react/src/components/TextHighlighter/__tests__/TextHighlighter.test.js
@@ -7,13 +7,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 
 import { TextHighlighter } from '../components/TextHighlighter';
-jest.mock('./button.scss', () => ({}));
+jest.mock('./text-highlighter.scss', () => ({}));
 describe('TextHighlighter', () => {
   describe('renders as expected - Component API', () => {
     it('should match snapshot', () => {
@@ -21,43 +20,6 @@ describe('TextHighlighter', () => {
       expect(container).toMatchSnapshot();
     });
 
-    it('renders a button element', () => {
-      render(<TextHighlighter />);
-      expect(screen.getByRole('button')).toBeInTheDocument();
-    });
 
-    it('renders with default text "Button"', () => {
-      render(<TextHighlighter />);
-      expect(screen.getByRole('button')).toHaveTextContent(/^Button$/);
-    });
-
-    it('supports a custom class name', () => {
-      const { container } = render(<TextHighlighter className="test" />);
-      expect(container.firstChild.firstChild).toHaveClass('test');
-    });
-
-    it('supports additional props', () => {
-      render(<TextHighlighter data-testid="test-button" />);
-      expect(screen.getByRole('button')).toHaveAttribute(
-        'data-testid',
-        'test-button'
-      );
-    });
-
-    it('supports disabled state', () => {
-      render(<TextHighlighter disabled />);
-      expect(screen.getByRole('button')).toBeDisabled();
-    });
-  });
-
-  describe('behaves as expected', () => {
-    it('calls onClick handler when clicked', async () => {
-      const onClick = jest.fn();
-      render(<TextHighlighter onClick={onClick} />);
-
-      expect(onClick).toHaveBeenCalledTimes(0);
-      await userEvent.click(screen.getByRole('button'));
-      expect(onClick).toHaveBeenCalledTimes(1);
-    });
   });
 });

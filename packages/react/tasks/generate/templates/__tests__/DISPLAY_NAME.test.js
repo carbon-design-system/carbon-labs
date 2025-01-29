3f1e6ffb077ba41170629abda7e7fb93cfe17dab
Anna Wen <54281166+annawen1@users.noreply.github.com>
2025-01-29 15:50:02 -0500

    chore: change react template test to one snapshot test (#387)
    * chore: change react template test to one snapshot test
    
    * chore: add test steps
    
    * chore: format

--- packages/react/tasks/generate/templates/__tests__/DISPLAY_NAME.test.js
+++ packages/react/tasks/generate/templates/__tests__/DISPLAY_NAME.test.js
@@ -7,57 +7,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 
 import { DISPLAY_NAME } from '../components/DISPLAY_NAME';
-jest.mock('./button.scss', () => ({}));
+jest.mock('./STYLE_NAME.scss', () => ({}));
 describe('DISPLAY_NAME', () => {
   describe('renders as expected - Component API', () => {
     it('should match snapshot', () => {
       const { container } = render(<DISPLAY_NAME />);
       expect(container).toMatchSnapshot();
-    });
-
-    it('renders a button element', () => {
-      render(<DISPLAY_NAME />);
-      expect(screen.getByRole('button')).toBeInTheDocument();
-    });
-
-    it('renders with default text "Button"', () => {
-      render(<DISPLAY_NAME />);
-      expect(screen.getByRole('button')).toHaveTextContent(/^Button$/);
-    });
-
-    it('supports a custom class name', () => {
-      const { container } = render(<DISPLAY_NAME className="test" />);
-      expect(container.firstChild.firstChild).toHaveClass('test');
-    });
-
-    it('supports additional props', () => {
-      render(<DISPLAY_NAME data-testid="test-button" />);
-      expect(screen.getByRole('button')).toHaveAttribute(
-        'data-testid',
-        'test-button'
-      );
-    });
-
-    it('supports disabled state', () => {
-      render(<DISPLAY_NAME disabled />);
-      expect(screen.getByRole('button')).toBeDisabled();
-    });
-  });
-
-  describe('behaves as expected', () => {
-    it('calls onClick handler when clicked', async () => {
-      const onClick = jest.fn();
-      render(<DISPLAY_NAME onClick={onClick} />);
-
-      expect(onClick).toHaveBeenCalledTimes(0);
-      await userEvent.click(screen.getByRole('button'));
-      expect(onClick).toHaveBeenCalledTimes(1);
     });
   });
 });

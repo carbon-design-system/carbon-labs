13f90387b33f4cf58153a51f1ed78826bbdbcc41
Alison Joseph <alison.joseph@us.ibm.com>
2025-01-31 08:49:01 -0600

    chore: fix ci

--- packages/react/src/components/UIShell/__tests__/SideNav-test.js
+++ packages/react/src/components/UIShell/__tests__/SideNav-test.js
@@ -6,7 +6,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+/* eslint-disable jsdoc/require-jsdoc */
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 

01832e97d083f5680d37ddaf7a337df83eebed54
Drew Glapa <glapadre@msu.edu>
2025-02-14 10:53:58 -0600

    feat(animated-header): add new animated header component (#406)
    * feat(animated-header): add new animated header component
    
    * chore(package): add react to package name
    
    * feat(animated-header): update copyright to 2025
    
    * feat(animated-header): update lockfile
    
    * feat(animated-header): update test snapchat
    
    * feat(animated-header): add example app
    
    * feat(animated-header): example app update license to 2025
    
    * feat(animated-header): lint cleanup
    
    * feat(animated-header): prettier format
    
    * feat(animated-header): remove watsonx lottie animation and replace with watsonx.data
    
    * feat(animated-header): move dependencies in AnimatedHeader/package.json
    
    * feat(animated-header): RollupError and circular dependencies cleanup
    
    * feat(animated-header): autoprefixing cleanup
    
    ---------
    
    Co-authored-by: Jeff Chew <jeff.chew@gmail.com>

---
+++ examples/react/AnimatedHeader/src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { AnimatedHeader } from '@carbon-labs/react-animated-header/es/index';
+
+function App() {
+  return <AnimatedHeader />;
+}
+
+export default App;

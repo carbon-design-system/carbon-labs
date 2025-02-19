40180a407c3f1d2563d0982b082c50cf200ca2f0
Maciej Stokfisz <39484495+IronZDev@users.noreply.github.com>
2025-02-19 18:26:07 +0100

    chore(animated-header): Fix missing assets (#450)
    * chore(animated-header): Fix missing assets
    
    * Moved SVG plugin to dev deps
    
    * Bump version number
    
    * Remove local npmrc
    
    * Format
    
    * Renamed animated_header to animated-header SCSS
    
    * Update SCSS name in stories

--- packages/react/src/components/AnimatedHeader/index.ts
+++ packages/react/src/components/AnimatedHeader/index.ts
@@ -6,7 +6,8 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import AnimatedHeader from '../AnimatedHeader/components/AnimatedHeader/AnimatedHeader';
+import AnimatedHeader from './components/AnimatedHeader/AnimatedHeader';
 import { BaseTile } from './components/Tiles/index.js';
+export * from './assets';
 
 export { AnimatedHeader, BaseTile };

4bb71430bc47156b2b1391a36be10c704b0ae56f
Alison Joseph <alison.joseph@us.ibm.com>
2025-02-20 14:33:19 -0600

    chore: test

--- packages/react/src/components/UIShell/components/Link.tsx
+++ packages/react/src/components/UIShell/components/Link.tsx
@@ -4,7 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+test codeowners
 import PropTypes from 'prop-types';
 import React, {
   type ElementType,

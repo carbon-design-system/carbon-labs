ec8c1c97cb04bcc773d104dfe39f767c1c880360
Ignacio Becerra <i1becerr@ucsd.edu>
2025-03-13 11:31:43 -0700

    feat(MDXComponents): add card components

--- packages/react/src/components/MDXComponents/index.ts
+++ packages/react/src/components/MDXComponents/index.ts
@@ -7,27 +7,16 @@
 export { Accordion } from './components/accordion/accordion.js';
 export { AnchorLink } from './components/anchor-links/anchor-link.js';
 export { AnchorLinks } from './components/anchor-links/anchor-links.js';
-
-// export {
-//   default as ArticleCard,
-//   ArticleCardProps,
-// } from './article-card/article-card.js';
+export { ArticleCard } from './components/article-card/article-card.js';
 export { ArtDirection } from './components/art-direction/art-direction.js';
 export { Aside } from './components/aside/aside.js';
 export { Caption } from './components/caption/caption.js';
-// export {
-//   default as CardGroup,
-//   CardGroupProps,
-// } from './card-group/card-group.js';
+export { CardGroup } from './components/card-group/card-group.js';
 export { Code } from './components/code/code.js';
 export { Divider } from './components/divider/divider.js';
 export { DoDont } from './components/do-dont/do-dont.js';
 export { DoDontRow } from './components/do-dont/do-dont-row.js';
 export { GifPlayer } from './components/gif-player/gif-player.js';
-// export {
-//   default as GifPlayer,
-//   GifPlayerProps,
-// } from './gif-player/gif-player.js';
 export { Column } from './components/grid-transform/column.js';
 export { Grid } from './components/grid-transform/grid.js';
 export { Row } from './components/grid-transform/row.js';
@@ -45,18 +34,11 @@ export { LI } from './components/markdown/li.js';
 export { OL } from './components/markdown/ol.js';
 export { P } from './components/markdown/p.js';
 export { UL } from './components/markdown/ul.js';
-// export { default as MiniCard, MiniCardProps } from './mini-card/mini-card.js';
-// export {
-//   default as PageDescription,
-//   PageDescriptionProps,
-// } from './page-description/page-description.js';
+export { MiniCard } from './components/mini-card/mini-card.js';
 export { PageDescription } from './components/page-description/page-description.js';
 export { PageTable } from './components/page-table/page-table.js';
 export { Preview } from './components/preview/preview.js';
-// export {
-//   default as ResourceCard,
-//   ResourceCardProps,
-// } from './resource-card/resource-card.js';
+export { ResourceCard } from './components/resource-card/resource-card.js';
 export { StorybookDemo } from './components/storybook-demo/storybook-demo.js';
 export { Tab } from './components/tabs/tab.js';
 export { Tabs } from './components/tabs/tabs.js';

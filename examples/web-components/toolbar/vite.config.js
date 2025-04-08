9900ef2c1862115477875a5373b101d39309aee3
devadula-nandan <devadula.nandan@gmail.com>
2025-04-08 18:28:52 +0530

    fix: lint, format, license

--- examples/web-components/toolbar/vite.config.js
+++ examples/web-components/toolbar/vite.config.js
@@ -1,11 +1,19 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, 'index.html'),
       },
     },
   },

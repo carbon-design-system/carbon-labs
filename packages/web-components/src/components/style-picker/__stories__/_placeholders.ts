de01d7a90c8b461beefeba702fa1d5a489728b61
Afsal K <makafsal@gmail.com>
2025-06-16 17:46:59 +0530

    chore: include license

--- packages/web-components/src/components/style-picker/__stories__/_placeholders.ts
+++ packages/web-components/src/components/style-picker/__stories__/_placeholders.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import * as carbonColors from '@carbon/colors';
 import Apple16 from '@carbon/web-components/es/icons/apple/16';
 import Corn16 from '@carbon/web-components/es/icons/corn/16';

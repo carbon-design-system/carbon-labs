d0c0698b4a762072c43374fd29799cdfead0b3d7
David Menendez <dmenend@us.ibm.com>
2025-08-13 13:20:12 -0500

    feat: style-picker react wrapper

---
+++ packages/web-components/src/components/style-picker/react/style-picker-section.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { createComponent } from '@lit/react';
+import StylePickerSection from '../components/style-picker-section/style-picker-section.js';
+
+export const CLABSStylePickerSection = createComponent({
+  tagName: 'clabs-style-picker-section',
+  elementClass: StylePickerSection,
+  react: React,
+  events: {},
+});

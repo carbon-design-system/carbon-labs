7db53f443c4499fd8877430685fc0014f918a6b7
Ariella Gilmore <ariellalgilmore@gmail.com>
2025-08-18 12:54:51 -0700

    feat(sidenav): new sideNavTitle (#752)
    * feat(ui-shell): add SideNavTitle
    
    * fix(side-nav-title): use title instead of children prop
    
    * fix(side-nav-title): hide spacing
    
    * fix(export): title
    
    * fix(format): update
    
    * fix(side-nav-title): div to li
    
    * fix(title): use children instead
    
    * fix(format): update
    
    * feat(sidenavtitle): update docs

--- packages/react/src/components/UIShell/__stories__/UIShell.stories.js
+++ packages/react/src/components/UIShell/__stories__/UIShell.stories.js
@@ -15,6 +15,7 @@ import { SideNavMenu } from '../components/SideNavMenu';
 import { SideNavMenuItem } from '../components/SideNavMenuItem';
 import { SideNavLink } from '../components/SideNavLink';
 import { SideNavSlot } from '../components/SideNavSlot';
+import { SideNavTitle } from '../components/SideNavTitle';
 import { Profile } from '../index';
 import {
   HeaderPopover,
@@ -100,6 +101,7 @@ export default {
     SideNavMenu,
     SideNavMenuItem,
     SideNavSlot,
+    SideNavTitle,
     TrialCountdown,
   },
   parameters: {
@@ -528,7 +530,7 @@ export const Demo = () => {
                     renderIcon={Layers}>
                     Infrastructure
                   </SideNavLink>
-                  <SideNavDivider />
+                  <SideNavTitle>Experience services</SideNavTitle>
                   <SideNavLink
                     href="http://www.carbondesignsystem.com"
                     renderIcon={Dashboard}>
@@ -696,7 +698,7 @@ export const Demo = () => {
                     renderIcon={Layers}>
                     Infrastructure
                   </SideNavLink>
-                  <SideNavDivider />
+                  <SideNavTitle>Experience services</SideNavTitle>
                   <SideNavLink
                     href="http://www.carbondesignsystem.com"
                     renderIcon={Dashboard}>
@@ -1012,7 +1014,7 @@ export const DemoPanel = () => {
                     renderIcon={Layers}>
                     Infrastructure
                   </SideNavLink>
-                  <SideNavDivider />
+                  <SideNavTitle>Experience services</SideNavTitle>
                   <SideNavLink
                     href="http://www.carbondesignsystem.com"
                     renderIcon={Dashboard}>
@@ -1151,7 +1153,7 @@ export const DemoPanel = () => {
                     renderIcon={Layers}>
                     Infrastructure
                   </SideNavLink>
-                  <SideNavDivider />
+                  <SideNavTitle>Experience services</SideNavTitle>
                   <SideNavLink
                     href="http://www.carbondesignsystem.com"
                     renderIcon={Dashboard}>

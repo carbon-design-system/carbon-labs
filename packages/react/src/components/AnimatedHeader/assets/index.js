cda7ab999152436ae0d1e3c214f11526ae51df5f
Drew-Glapa <drew.glapa@ibm.com>
2025-08-28 14:24:48 -0500

    feat(animated-header): update old static files

--- packages/react/src/components/AnimatedHeader/assets/index.js
+++ packages/react/src/components/AnimatedHeader/assets/index.js
@@ -6,30 +6,56 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import dataFabricAnimatedLight from './animated/header_1312x738_data_fabric_light_03.json';
-import dataFabricAnimatedDark from './animated/header_1312x738_data_fabric_dark_06.json';
-import dataFabricStaticLight from './static/illustration_data_fabric_light_04.svg';
-import dataFabricStaticDark from './static/illustration_data_fabric_dark_05.svg';
+
+// ------------ Static (SVG) ------------
+import dataFabricStaticLight from './static/illustration_data_fabric_light_05.svg';
+import dataFabricStaticDark from './static/illustration_data_fabric_dark_06.svg';
+
 import db2StaticLight from './static/illustration_db2_light.svg';
 import db2StaticDark from './static/illustration_db2_dark.svg';
+
+import watsonXStaticLight from './static/illustration_watsonx_data_light_06.svg';
+import watsonXStaticDark from './static/illustration_watsonx_data_dark_05.svg';
+
+import watsonXAStaticLight from './static/illustration_watsonx_a_light_01.svg';
+import watsonXAStaticDark from './static/illustration_watsonx_a_dark_01.svg';
+
+import wxbiaStaticLight from './static/illustration_wxbia_light_01.svg';
+import wxbiaStaticDark from './static/illustration_wxbia_dark_01.svg';
+
+// ------------ Animated (JSON/Lottie) ------------
+import dataFabricAnimatedLight from './animated/header_1312x738_data_fabric_light_03.json';
+import dataFabricAnimatedDark from './animated/header_1312x738_data_fabric_dark_06.json';
+
 import watsonXAnimatedLight from './animated/header_1312x738_watsonx_data_light_03.json';
 import watsonXAnimatedDark from './animated/header_1312x738_watsonx_data_dark_02.json';
-import watsonXStaticLight from './static/illustration_watson_x_data_light_03.svg';
-import watsonXStaticDark from './static/illustration_watson_x_data_dark_03.svg';
+
+import watsonXAAnimatedLight from './animated/header_illustration_container_watsonx_a_light_06.json';
+import watsonXAAnimatedDark from './animated/header_illustration_container_watsonx_a_dark_06.json';
+
 import wxbiaAnimatedLight from './animated/header_illustration_container_wxbia_light_08.json';
 import wxbiaAnimatedDark from './animated/header_illustration_container_wxbia_dark_05.json';
 
 export {
+  // Animated
   dataFabricAnimatedLight,
   dataFabricAnimatedDark,
+  watsonXAnimatedLight,
+  watsonXAnimatedDark,
+  watsonXAAnimatedLight,
+  watsonXAAnimatedDark,
+  wxbiaAnimatedLight,
+  wxbiaAnimatedDark,
+
+  // Static
   dataFabricStaticLight,
   dataFabricStaticDark,
   db2StaticLight,
   db2StaticDark,
-  watsonXAnimatedLight,
-  watsonXAnimatedDark,
   watsonXStaticLight,
   watsonXStaticDark,
-  wxbiaAnimatedLight,
-  wxbiaAnimatedDark,
+  watsonXAStaticLight,
+  watsonXAStaticDark,
+  wxbiaStaticLight,
+  wxbiaStaticDark,
 };

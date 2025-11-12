d23243b990a4a7d033cc8544a6c91de27f60cbd1
Rory Mullan <Rmullan@uk.ibm.com>
2025-11-12 12:03:51 +0000

    stupid yarn lock

--- packages/web-components/src/components/global-header/components/global-header/src/components/HybridIpaasHeader/__stories__/HybridIPaasHeader.stories.ts
+++ packages/web-components/src/components/global-header/components/global-header/src/components/HybridIpaasHeader/__stories__/HybridIPaasHeader.stories.ts
@@ -167,7 +167,7 @@ export const WithSolis: Story = {
       <clabs-global-header-hybrid-ipaas
         productName="App Connect"
         productKey="appconnect"
-       solisEnabled="true"></clabs-global-header-hybrid-ipaas>
+        solisEnabled="true"></clabs-global-header-hybrid-ipaas>
     </div>
   `,
 };

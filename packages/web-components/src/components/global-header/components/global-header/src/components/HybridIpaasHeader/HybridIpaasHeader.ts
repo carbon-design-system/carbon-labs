0a6aeb7ef06da7866e5f110cc85bc62fb29a59f4
Rory Mullan <Rmullan@uk.ibm.com>
2026-01-22 10:18:05 +0000

    stupid format

--- packages/web-components/src/components/global-header/components/global-header/src/components/HybridIpaasHeader/HybridIpaasHeader.ts
+++ packages/web-components/src/components/global-header/components/global-header/src/components/HybridIpaasHeader/HybridIpaasHeader.ts
@@ -181,7 +181,7 @@ export class HybridIpaasHeader extends LitElement {
         is_prod: isProd,
         cdn_hostname: SOLIS_CDN_HOSTNAMES[env],
         deployment_environment: solisDeploymentEnvironment[env] || 'local',
-        product_id: 'ipaas'
+        product_id: 'ipaas',
       };
     } else {
       return {
@@ -190,7 +190,7 @@ export class HybridIpaasHeader extends LitElement {
         is_prod: isProd,
         cdn_hostname: SOLIS_CDN_HOSTNAMES[env],
         deployment_environment: solisDeploymentEnvironment[env] || 'local',
-        product_id: 'ipaas'
+        product_id: 'ipaas',
       };
     }
   }

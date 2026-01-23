91fecfe125194f138773061fc52437dde3ae1971
Sarah Hewitt <CARNASAR@uk.ibm.com>
2026-01-23 16:47:03 +0000

    Formatting

--- packages/web-components/src/components/global-header/codecept.conf.ts
+++ packages/web-components/src/components/global-header/codecept.conf.ts
@@ -1,32 +1,32 @@
 exports.config = {
-	tests: './components/global-header/src/components/HybridIpaasHeader/__codecept__/*.spec.js',
-	output: './codecept/screenshots',
-	helpers: {
-		WebDriver: {
-			url: 'http://localhost:6007',
-			show: true,
-			browser: 'chrome',
-			desiredCapabilities: {
-				chromeOptions: {
-					args: [ "--headless", "--disable-gpu", "--no-sandbox" ]
-				}
-			}
-		}
-	},
-	bootstrap: null,
-	mocha: {},
-	name: 'wc-global-header',
-	plugins: {
-		pauseOnFail: {
+  tests:
+    './components/global-header/src/components/HybridIpaasHeader/__codecept__/*.spec.js',
+  output: './codecept/screenshots',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost:6007',
+      show: true,
+      browser: 'chrome',
+      desiredCapabilities: {
+        chromeOptions: {
+          args: ['--headless', '--disable-gpu', '--no-sandbox'],
         },
-		retryFailedStep: {
-			enabled: true
-		},
-		tryTo: {
-			enabled: true
-		},
-		screenshotOnFail: {
-			enabled: true
-		}
-	}
+      },
+    },
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'wc-global-header',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
 };

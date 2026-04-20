eb445dc9375a470a57e378181531a67ccb8865eb
Vigneswara Rao Gowrabathuni <vigneswara.gowrabathuni@ibm.com>
2026-04-20 22:25:35 +0530

    fix(uishell): added aria-hidden attribute for SharkFinIcon (#1185)

--- packages/react/src/components/UIShell/components/SharkFinIcon.tsx
+++ packages/react/src/components/UIShell/components/SharkFinIcon.tsx
@@ -44,6 +44,7 @@ export const SharkFinIcon: React.FC<SharkFinIconProps> = ({
       width="4"
       height="4"
       viewBox="0 0 4 4"
+      aria-hidden="true"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_519_52879)">

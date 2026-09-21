5788b53cacfd56d22e34b64c0e41b48eeb4bfe0d
hurwal <160017246+hurwal@users.noreply.github.com>
2026-09-21 15:32:25 +0100

    Update HybridIpaasHeader.ts

--- packages/web-components/src/components/global-header/components/global-header/src/components/HybridIpaasHeader/HybridIpaasHeader.ts
+++ packages/web-components/src/components/global-header/components/global-header/src/components/HybridIpaasHeader/HybridIpaasHeader.ts
@@ -84,7 +84,7 @@ export class HybridIpaasHeader extends LitElement {
     ...INITIAL_AUTOMATION_HEADER_PROPS,
     brand: {
       company: 'IBM',
-      product: 'Integration',
+      product: '',
     },
     capabilityName: {
       label: '',

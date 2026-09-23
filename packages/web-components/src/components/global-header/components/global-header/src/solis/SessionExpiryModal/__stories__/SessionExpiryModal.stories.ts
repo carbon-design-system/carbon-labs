9e852c219d088f22bf39102397e694d20e6fa40a
Sarah Hewitt <CARNASAR@uk.ibm.com>
2026-09-23 17:03:01 +0100

    Add totalSeconds to storybook

--- packages/web-components/src/components/global-header/components/global-header/src/solis/SessionExpiryModal/__stories__/SessionExpiryModal.stories.ts
+++ packages/web-components/src/components/global-header/components/global-header/src/solis/SessionExpiryModal/__stories__/SessionExpiryModal.stories.ts
@@ -27,7 +27,7 @@ type Story = StoryObj<typeof SessionExpiryModal>;
 export const Basic: Story = {
   render: () => html`
     <div role="main">
-      <clabs-global-header-session-expiry-modal expiryTime="5 minutes">
+      <clabs-global-header-session-expiry-modal open totalSeconds="${300}">
       </clabs-global-header-session-expiry-modal>
     </div>
   `,
